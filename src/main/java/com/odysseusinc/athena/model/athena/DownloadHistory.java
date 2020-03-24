ff44cb0f3c1a319ee267f052d0deb3c2cd933070
Alex Cumarav <7705583+acumarav@users.noreply.github.com>
2020-03-24 15:03:31 +0100

    log downloads to DB (#115)
    * Athena - log downloads to DB
    
    Co-authored-by: alexandr cumarav <https://localhost:8080/study-manager/studies/6>

---
+++ src/main/java/com/odysseusinc/athena/model/athena/DownloadHistory.java
@@ -0,0 +1,109 @@
+/*
+ *
+ * Copyright 2020 Odysseus Data Services, inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Company: Odysseus Data Services, Inc.
+ * Product Owner/Architecture: Gregory Klebanov
+ * Authors: Alexandr Cumarav
+ * Created: March 24, 2020
+ *
+ */
+
+package com.odysseusinc.athena.model.athena;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "download_history")
+public class DownloadHistory {
+    @Id
+    @SequenceGenerator(name = "download_history_pk_sequence", sequenceName = "download_history_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "download_history_pk_sequence")
+    private Long id;
+    @ManyToOne(optional = false, targetEntity = DownloadBundle.class)
+    @JoinColumn(name = "bundle_id")
+    private DownloadBundle vocabularyBundle;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "download_time")
+    private Date downloadTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DownloadBundle getVocabularyBundle() {
+        return vocabularyBundle;
+    }
+
+    public void setVocabularyBundle(DownloadBundle vocabularyBundle) {
+        this.vocabularyBundle = vocabularyBundle;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Date getDownloadTime() {
+        return downloadTime;
+    }
+
+    public void setDownloadTime(Date downloadTime) {
+        this.downloadTime = downloadTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DownloadHistory that = (DownloadHistory) o;
+        return id.equals(that.id) &&
+                vocabularyBundle.equals(that.vocabularyBundle) &&
+                userId.equals(that.userId) &&
+                downloadTime.equals(that.downloadTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "DownloadHistory{" +
+                "id=" + id +
+                ", vocabularyBundle=" + vocabularyBundle +
+                ", userId=" + userId +
+                ", downloadTime=" + downloadTime +
+                '}';
+    }
+}
---
+++ src/main/java/com/odysseusinc/athena/service/impl/DownloadsHistoryServiceImpl.java
@@ -0,0 +1,55 @@
+/*
+ *
+ * Copyright 2020 Odysseus Data Services, inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Company: Odysseus Data Services, Inc.
+ * Product Owner/Architecture: Gregory Klebanov
+ * Authors: Alexandr Cumarav
+ * Created: March 20, 2020
+ *
+ */
+
+package com.odysseusinc.athena.service.impl;
+
+import com.odysseusinc.athena.model.athena.DownloadBundle;
+import com.odysseusinc.athena.model.athena.DownloadHistory;
+import com.odysseusinc.athena.repositories.athena.DownloadHistoryRepository;
+import com.odysseusinc.athena.service.DownloadsHistoryService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Transactional
+@Service
+public class DownloadsHistoryServiceImpl implements DownloadsHistoryService {
+
+    private final DownloadHistoryRepository downloadHistoryRepository;
+
+    public DownloadsHistoryServiceImpl(DownloadHistoryRepository downloadHistoryRepository) {
+
+        this.downloadHistoryRepository = downloadHistoryRepository;
+    }
+
+    @Override
+    public void updateStatistics(DownloadBundle bundle, Long userId) {
+
+        DownloadHistory downloadRecord = new DownloadHistory();
+        downloadRecord.setUserId(userId);
+        downloadRecord.setVocabularyBundle(bundle);
+        downloadRecord.setDownloadTime(new Date());
+
+        downloadHistoryRepository.save(downloadRecord);
+    }
+}
