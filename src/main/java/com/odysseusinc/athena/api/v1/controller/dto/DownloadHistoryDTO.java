4b0c8bc3e7a0325962a9757f57b5cf1ea43340d5
Alex Cumarav <7705583+acumarav@users.noreply.github.com>
2020-04-30 13:07:55 +0200

    statistics changes (#134)
    * statistics changes
    
    * Statistics changes - convert to lombok
    
    Co-authored-by: alexandr cumarav <https://localhost:8080/study-manager/studies/6>

--- src/main/java/com/odysseusinc/athena/api/v1/controller/dto/DownloadHistoryDTO.java
+++ src/main/java/com/odysseusinc/athena/api/v1/controller/dto/DownloadHistoryDTO.java
@@ -24,83 +24,23 @@
 package com.odysseusinc.athena.api.v1.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
+import static com.odysseusinc.athena.model.common.AthenaConstants.COMMON_DATE_FORMAT;
+
+@Data
 public class DownloadHistoryDTO {
-    private String vocabularyName;
     private String code;
+    private String email;
     private String userName;
     private String organization;
     private LocalDateTime date;
 
-    public String getVocabularyName() {
-        return vocabularyName;
-    }
 
-    public void setVocabularyName(String vocabularyName) {
-        this.vocabularyName = vocabularyName;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getOrganization() {
-        return organization;
-    }
-
-    public void setOrganization(String organization) {
-        this.organization = organization;
-    }
-
-    @JsonFormat(pattern = "dd-MMM-yyyy")
+    @JsonFormat(pattern = COMMON_DATE_FORMAT)
     public LocalDateTime getDate() {
         return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        DownloadHistoryDTO that = (DownloadHistoryDTO) o;
-        return Objects.equals(vocabularyName, that.vocabularyName) &&
-                Objects.equals(code, that.code) &&
-                Objects.equals(userName, that.userName) &&
-                Objects.equals(organization, that.organization) &&
-                Objects.equals(date, that.date);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(vocabularyName, code, userName, organization, date);
-    }
-
-    @Override
-    public String toString() {
-        return "DownloadHistoryDTO{" +
-                ", vocabularyName='" + vocabularyName + '\'' +
-                ", code='" + code + '\'' +
-                ", userName='" + userName + '\'' +
-                ", organization='" + organization + '\'' +
-                ", date=" + date +
-                '}';
     }
 }
