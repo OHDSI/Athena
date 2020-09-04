760041b35d33ed61b473b649df37bb9c454d281a
Alex Cumarav <7705583+acumarav@users.noreply.github.com>
2020-09-04 09:44:53 +0200

    update version to 1.11.1 (#198)
    Co-authored-by: alexandr cumarav <https://localhost:8080/study-manager/studies/6>

--- src/main/docker/cpt4_4_5/cpt.sh
+++ src/main/docker/cpt4_4_5/cpt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# = 1 ]; then
-  java -Dumls-apikey=$1 -jar cpt4.jar 5
+  java -Dumls-apikey=$1 -jar cpt4.jar 4
 else
-  java -Dumls-user=$1 -Dumls-password=$2 -jar cpt4.jar 5
+  java -Dumls-user=$1 -Dumls-password=$2 -jar cpt4.jar 4
 fi
