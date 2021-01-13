7a1f5997585fc28b2e4704d04aab27e9d4a0ff3f
Alex Cumarav <7705583+acumarav@users.noreply.github.com>
2021-01-13 10:24:28 +0100

    update CPT4 util (#216)

--- src/main/docker/cpt4_4_5/cpt.bat
+++ src/main/docker/cpt4_4_5/cpt.bat
@@ -1,14 +1,3 @@
 @echo OFF
-IF NOT "%1"=="" IF NOT "%2"=="" GOTO PWD
-IF NOT "%1"=="" IF "%2"=="" GOTO APIKEY
-ECHO Please specify UMLS APIKEY or USERNAME and PASSWORD
-GOTO EOF
 
-:APIKEY
 java -Dumls-apikey=%1 -jar cpt4.jar 4
-GOTO EOF
-
-:PWD
-java -Dumls-user=%1 -Dumls-password=%2 -jar cpt4.jar 4
-
-:EOF
