dddf1ef2d54a7b8e1262eb8e4f301aef5112ddb4
alex cumarav <tsumarev@gmail.com>
2021-03-29 12:33:08 +0200

    update pac4j to version 3.3

--- src/main/java/com/odysseusinc/athena/service/mail/EmailSenderService.java
+++ src/main/java/com/odysseusinc/athena/service/mail/EmailSenderService.java
@@ -23,7 +23,7 @@
 package com.odysseusinc.athena.service.mail;
 
 import com.odysseusinc.athena.exceptions.AthenaException;
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
