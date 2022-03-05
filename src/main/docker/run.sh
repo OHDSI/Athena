fee5567b6ccfbd4c2baee5c8a4410696b0debc40
Dan Kim <deekim@users.noreply.github.com>
2022-03-05 05:09:51 -0500

    Setup application.properties resource
    for use in Docker

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-java -Djava.security.egd=file:/dev/./urandom -jar athena.jar
+java -Djava.security.egd=file:/dev/./urandom -jar athena.jar --spring.config.additional-location=application.properties
 exit 0
