514894e6ca5070bec7350cbf63347eb46a43a066
Juan Carlos Ñamendi Pineda <juan.namendi@odysseusinc.com>
2022-08-16 06:43:04 -0600

    Fix missing IF NOT EXISTS line in the script.

--- src/main/resources/db/migration/athena/V20220810093200__schema_create_index_download_item.sql
+++ src/main/resources/db/migration/athena/V20220810093200__schema_create_index_download_item.sql
@@ -1,2 +1,2 @@
-CREATE INDEX idx_download_bundle_id on download_item(download_bundle_id);
-CREATE INDEX idx_vocabulary_id_v4 on download_item(vocabulary_id_v4);
+CREATE INDEX IF NOT EXISTS idx_download_bundle_id on download_item(download_bundle_id);
+CREATE INDEX IF NOT EXISTS idx_vocabulary_id_v4 on download_item(vocabulary_id_v4);
