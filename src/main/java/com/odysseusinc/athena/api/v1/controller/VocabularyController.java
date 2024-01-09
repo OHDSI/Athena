d26d9d1a4b9aa77159054f65b7fbd08523cb6bbd
ymolodkov <yaroslav.molodkov@odysseusinc.com>
2024-01-09 13:00:36 +0100

    AVD-28: create an endpoint that returns mock data for release VOC versions. It should be replaced with the real data

--- src/main/java/com/odysseusinc/athena/api/v1/controller/VocabularyController.java
+++ src/main/java/com/odysseusinc/athena/api/v1/controller/VocabularyController.java
@@ -25,6 +25,7 @@ package com.odysseusinc.athena.api.v1.controller;
 import com.odysseusinc.athena.api.v1.controller.dto.CustomPageImpl;
 import com.odysseusinc.athena.api.v1.controller.dto.LicenseExceptionDTO;
 import com.odysseusinc.athena.api.v1.controller.dto.PageDTO;
+import com.odysseusinc.athena.api.v1.controller.dto.vocabulary.*;
 import com.odysseusinc.athena.api.v1.controller.dto.vocabulary.AcceptDTO;
 import com.odysseusinc.athena.api.v1.controller.dto.vocabulary.AddingUserLicensesDTO;
 import com.odysseusinc.athena.api.v1.controller.dto.vocabulary.DownloadShareChangeDTO;
@@ -37,34 +38,26 @@ import com.odysseusinc.athena.exceptions.PermissionDeniedException;
 import com.odysseusinc.athena.exceptions.ValidationException;
 import com.odysseusinc.athena.model.athena.DownloadBundle;
 import com.odysseusinc.athena.model.security.AthenaUser;
+import com.odysseusinc.athena.service.*;
+import com.odysseusinc.athena.service.impl.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.security.Principal;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 import static com.odysseusinc.athena.util.CDMVersion.getByValue;
 import static com.odysseusinc.athena.util.CDMVersion.notExist;
-import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
-import static org.apache.commons.lang.StringUtils.isEmpty;
 
 @Tag(name = "VocabularyController")
 @RestController
@@ -216,7 +209,6 @@ public class VocabularyController extends AbstractVocabularyController {
     }
 
 
-
     @GetMapping("/release-version")
     public VocabularyVersionDTO releaseVersion() {
 
@@ -224,4 +216,13 @@ public class VocabularyController extends AbstractVocabularyController {
 
         return converterUtils.convert(vocabularyVersion, VocabularyVersionDTO.class);
     }
+
+    @GetMapping("/vocabulary-release-versions")
+    public List<VocabularyReleaseVersionDTO> releaseVersions() {
+        return Arrays.asList(
+                new VocabularyReleaseVersionDTO("20230831", "v20230831", true),
+                new VocabularyReleaseVersionDTO("20230131", "v20230131", false)
+        );
+    }
+
 }
---
+++ src/main/java/com/odysseusinc/athena/api/v1/controller/dto/vocabulary/VocabularyReleaseVersionDTO.java
@@ -0,0 +1,20 @@
+package com.odysseusinc.athena.api.v1.controller.dto.vocabulary;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class VocabularyReleaseVersionDTO {
+    private final String value;
+    private final String label;
+    private final boolean current;
+
+    //TODO DEV this suppose to be process on the front-end
+    public String getLabel() {
+        if (current) {
+            return String.format("%s(current)", label);
+        }
+        return label;
+    }
+}
